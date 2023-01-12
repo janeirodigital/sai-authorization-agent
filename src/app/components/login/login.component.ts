f94c1e16507877fa2095bf9e233ce64d76c39543
mariano-romero-jd <mariano.romero@janeirodigital.com>
2023-01-12 13:31:05 -0600

    UI and server login validation

--- src/app/components/login/login.component.ts
+++ src/app/components/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { UntypedFormControl, UntypedFormGroup } from '@angular/forms';
 import {Store} from "@ngrx/store";
 import {CoreActions} from "../../actions";
